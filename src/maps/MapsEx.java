a7e3ab20294e467fa4764b387a28e91f1402618f
sahilsaini18 <157702038+sahilsaini18@users.noreply.github.com>
2024-02-06 14:49:20 +0530

    MapsEx.java

--- src/maps/MapsEx.java
+++ src/maps/MapsEx.java
@@ -1,20 +1,24 @@
 package maps;
 
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Logger;
 
 public class MapsEx {
+	private static final Logger log = Logger.getLogger(MapsEx.class.getName());
+
 	/**
-	 * @param args Insertion in map is demonstrated below
-	 * 
+	 * @param args Insertion in the map is demonstrated below
 	 */
 	public static void main(String[] args) {
 		Map<Integer, String> mp = new HashMap<Integer, String>();
 		mp.put(100, "amit");
 		mp.put(200, "vishal");
 		mp.put(300, "vinay");
-		// Iterating the map and printing the key value pair
-		for (Map.Entry m : mp.entrySet()) {
-			System.out.println(m.getKey() + " " + m.getValue());
+
+		// Iterating the map and printing the key-value pair
+		for (Map.Entry<Integer, String> entry : mp.entrySet()) {
+			log.info(entry.getKey() + " " + entry.getValue());
 		}
 	}
 }
